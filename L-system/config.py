da5b0f01fdd4505830c58734442add6ee1ffe8bf
Qu1nel <quinelleave@gmail.com>
2021-12-05 06:00:49 +0300

    added rendering of the following shapes: Gosper curve Flower Bush + made a file with drawing functions

--- L-system/config.py
+++ L-system/config.py
@@ -1,12 +1,15 @@
 WIDTH = 1920
 HEIGHT = 1000
 
-gens = 13
+gens = 11
 rules = {
     'Honeycombs': ({'A': 'AB', 'B': 'A'}, 'A'),
     'Sierpinski triangle': ({'F': 'F-G+F+G-F', 'G': 'GG'}, 'F'),
     'Dragon curve': ({'X': 'X+YF+', 'Y': '-FX-Y'}, 'XY'),
     'Koch snowflake': ({'F': 'F-F++F-F'}, 'F++F++F'),
     'Tree': ({'X': 'F[+X]F[-X]+X', 'F': 'FF'}, 'X'),
-    'Realistic Tree': ({'X': 'F[@[-X]+X]'}, 'X')
+    'Realistic Tree': ({'X': 'F[@[-X]+X]'}, 'X'),
+    'Gosper curve': ({'F': '', 'L': 'FL-FR--FR+FL++FLFL+FR-', 'R': '+FL-FRFR--FR-FL++FL+FR'}, 'FL'),
+    'Flower': ({'F': 'FF[++F][+F][F][-F][--F]'}, 'F[+F+F][-F-F][++F][--F]F'),
+    'Bush': ({'F': '-F+F+[+F-F-]-[-F+F+F]'}, 'F'),
 }
--- L-system/main.py
+++ L-system/main.py
@@ -1,7 +1,7 @@
 import turtle
-from random import randint
 
 import config
+import drawings as draw
 
 
 class Rules(object):
@@ -86,120 +86,26 @@ class App(object):
         return self.__kevin
 
     def draw(self) -> None:
-        def _honeycombs(obj):
-            angle = 60
-            step = 50
-            for char in obj.l_system.axiom:
-                if char == obj.l_system.rules[0]:
-                    obj.turtle.left(angle)
-                    obj.turtle.forward(step)
-                elif char == obj.l_system.rules[1]:
-                    obj.turtle.right(angle)
-                    obj.turtle.forward(step)
-
-        def _sierpinski_triangle(obj):
-            angle = 120
-            step = 8
-            for char in obj.l_system.axiom:
-                if char in (obj.l_system.rules[0], obj.l_system.rules[1]):
-                    obj.turtle.forward(step)
-                elif char == '+':
-                    obj.turtle.right(angle)
-                elif char == '-':
-                    obj.turtle.left(angle)
-
-        def _dragon_curve(obj):
-            angle = 90
-            step = 4
-            for char in obj.l_system.axiom:
-                if char in (obj.l_system.rules[0], obj.l_system.rules[1]):
-                    obj.turtle.forward(step)
-                elif char == '+':
-                    obj.turtle.right(angle)
-                elif char == '-':
-                    obj.turtle.left(angle)
-
-        def _koch_snowflake(obj):
-            angle = 60
-            step = 5
-            for char in obj.l_system.axiom:
-                if char == obj.l_system.rules[0]:
-                    obj.turtle.forward(step)
-                elif char == '+':
-                    obj.turtle.right(angle)
-                elif char == '-':
-                    obj.turtle.left(angle)
-
-        def _tree(obj):
-            step = 5
-            angle = 22.5
-            stack = []
-            obj.turtle.left(90)
-            obj.turtle.goto((0, -300))
-            for char in obj.l_system.axiom:
-                if char == obj.l_system.rules[1]:
-                    obj.turtle.forward(step)
-                elif char == '+':
-                    obj.turtle.right(angle)
-                elif char == '-':
-                    obj.turtle.left(angle)
-                elif char == '[':
-                    angle_, pos_ = obj.turtle.heading(), obj.turtle.pos()
-                    stack.append((angle_, pos_))
-                elif char == ']':
-                    angle_, pos_ = stack.pop()
-                    obj.turtle.setheading(angle_)
-                    obj.turtle.penup()
-                    obj.turtle.goto(pos_)
-                    obj.turtle.pendown()
-
-        def _realtree(obj):
-            step = 90
-            angle = lambda: randint(0, 38)
-            color = [0.35, 0.2, 0.0]
-            thickness = 20
-            stack = []
-            obj.turtle.left(90)
-            obj.turtle.pensize(thickness)
-            for char in obj.l_system.axiom:
-                obj.turtle.color(color)
-                if char in ('X', 'F'):
-                    obj.turtle.forward(step)
-                elif char == '@':
-                    step -= 6
-                    color[1] += 0.04
-                    thickness -= 2
-                    thickness = max(1, thickness)
-                    obj.turtle.pensize(thickness)
-                elif char == '+':
-                    obj.turtle.right(angle())
-                elif char == '-':
-                    obj.turtle.left(angle())
-                elif char == '[':
-                    angle_, pos_ = obj.turtle.heading(), obj.turtle.pos()
-                    stack.append((angle_, pos_, thickness, step, color[1]))
-                elif char == ']':
-                    angle_, pos_, thickness, step, color[1] = stack.pop()
-                    obj.turtle.pensize(thickness)
-                    obj.turtle.setheading(angle_)
-                    obj.turtle.penup()
-                    obj.turtle.goto(pos_)
-                    obj.turtle.pendown()
-
         self.l_system.get_result()
 
         if self.mode.lower() == 'honeycombs':
-            _honeycombs(self)
+            draw._honeycombs(self)
         elif self.mode.lower() == 'sierpinski triangle':
-            _sierpinski_triangle(self)
+            draw._sierpinski_triangle(self)
         elif self.mode.lower() == 'dragon curve':
-            _dragon_curve(self)
+            draw._dragon_curve(self)
         elif self.mode.lower() == 'koch snowflake':
-            _koch_snowflake(self)
+            draw._koch_snowflake(self)
         elif self.mode.lower() == 'tree':
-            _tree(self)
-        if self.mode.lower() == 'realistic tree':
-            _realtree(self)
+            draw._tree(self)
+        elif self.mode.lower() == 'realistic tree':
+            draw._realtree(self)
+        elif self.mode.lower() == 'gosper curve':
+            draw._gosper_curve(self)
+        elif self.mode.lower() == 'flower':
+            draw._flower(self)
+        elif self.mode.lower() == 'bush':
+            draw._bush(self)
 
     def run(self) -> None:
         self.draw()
@@ -208,5 +114,5 @@ class App(object):
 
 if __name__ == '__main__':
     app = App()
-    app.rules = 'Sierpinski triangle'  # all rules in the config (module) in the rules (dict) + change gens
+    app.rules = 'Realistic Tree'  # all rules in the config (module) in the rules (dict) + change gens
     app.run()
