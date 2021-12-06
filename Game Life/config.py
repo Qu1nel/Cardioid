7e3f8fa2b5c9efabd513b322a5c4811746411a42
Qu1nel <quinelleave@gmail.com>
2021-12-06 14:44:25 +0300

    added a class "GameLife"

--- Game Life/config.py
+++ Game Life/config.py
@@ -1,2 +1,7 @@
+# window settings
 WIDTH = 1600
 HEIGHT = 900
+
+# GameLife settings
+COLOR_CELL = (241, 196, 15)  # yellow almost
+FRAMERATE = 120
--- Game Life/main.py
+++ Game Life/main.py
@@ -1,6 +1,14 @@
+from sys import exit
+
 import pygame as pg
 
 import config as c
+
+
+class GameLife(object):
+    def __init__(self, sc: pg.Surface):
+        self.sc = sc
+        self.color_cell = c.COLOR_CELL
 
 
 class App(object):
@@ -8,16 +16,27 @@ class App(object):
         self.width, self.height = (c.WIDTH, c.HEIGHT)
         self.screen = pg.display.set_mode((c.WIDTH, c.HEIGHT))
         self.clock = pg.time.Clock()
-        self.game_life = None
+        self.game_life = GameLife(self.screen)
 
-    def handle_events(self):
+    @staticmethod
+    def handle_events() -> None:
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                pg.quit()
+                exit()
+
+    def process(self) -> None:
         pass
 
-    def process(self):
+    def draw(self) -> None:
         pass
 
-    def run(self):
-        pass
+    def run(self) -> None:
+        while True:
+            self.draw()
+            self.process()
+            self.handle_events()
+            self.clock.tick(c.FRAMERATE)
 
 
 def main() -> None:
