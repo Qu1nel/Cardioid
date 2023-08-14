017f1044db71e1fc617f93f811b047aae77f41e4
Ivan Kovach <eeiprex@gmail.com>
2023-08-14 23:18:17 +0300

    [feat#__init__] Added exe support
    Added resource_path() function for correct path to application resources.
    
    Added icon and name for the application.

--- src/__init__.py
+++ src/__init__.py
@@ -1,10 +1,28 @@
 """Cardioid - visualization of an interesting version of this curve in animation."""
+import sys
+from pathlib import Path
 
 import pygame as pg  # type: ignore
 
 from src.main import App
 
+
+def resource_path(relative_path: Path) -> Path:
+    """Function for working paths inside an exe for python."""
+    base_path = Path(getattr(sys, "_MEIPASS", ".")).absolute()
+    return base_path.joinpath(relative_path)
+
+
 pg.init()
+
+icon_name = "icon.png"
+icon_folder_name = "icons"
+icon_path = Path(icon_folder_name) / Path(icon_name)
+
+icon = pg.image.load(resource_path(icon_path))
+
+pg.display.set_icon(icon)
+pg.display.set_caption("Cardioid")
 
 
 def main() -> None:
