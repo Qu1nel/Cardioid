963dcecd05678a33f8a8a58992a941c177ed2f9c
Ivan Kovach <eeiprex@gmail.com>
2023-08-15 21:31:51 +0300

    [refact] Add `Position` class and move TApp to `misc.py`
    Rename and move class "TApp" to "misc/AppType".
    Also add class (namedtuple) "Position" for attr of Cardioid.position

--- src/main.py
+++ src/main.py
@@ -1,24 +1,10 @@
 import math
 import sys
-from typing import Never, Protocol
+from typing import Never
 
 import pygame as pg  # type: ignore
 
-from src.misc import Singleton
-
-
-class TApp(Protocol):
-    """Protocol for App object."""
-
-    screen: pg.SurfaceType
-
-    def run(self) -> None:
-        """Protocol method for run app."""
-        ...
-
-    def draw(self) -> None:
-        """Protocol method for drawing app on display."""
-        ...
+from src.misc import AppType, Position, Singleton
 
 
 class Cardioid(metaclass=Singleton):
@@ -38,19 +24,20 @@ class Cardioid(metaclass=Singleton):
 
     """
 
-    app: TApp
+    app: AppType
     radius: float
     num_lines: int
-    position: tuple[int, int]
+    position: Position
     counter: float
     inc: float
 
-    def __init__(self, app: TApp) -> None:
+    def __init__(self, app: AppType) -> None:
         """Initializing a cardioid object."""
         self.app = app
         self.radius = 350.0
         self.num_lines = 150
-        self.position = (self.app.screen.get_width() // 2, self.app.screen.get_height() // 2)
+        self.position = Position(x=self.app.screen.get_width() // 2, y=self.app.screen.get_height() // 2)
+
         self.counter = 0.0
         self.inc = 0.0
 
@@ -74,24 +61,21 @@ class Cardioid(metaclass=Singleton):
         factor = 1 + 0.001 * time
 
         def _get_x(delta: float) -> int:
-            return -int(self.radius * math.cos(delta)) + self.position[0]
+            return -int(self.radius * math.cos(delta)) + self.position.x
 
         def _get_y(delta: float) -> int:
-            return int(self.radius * math.sin(delta)) + self.position[1]
+            return int(self.radius * math.sin(delta)) + self.position.y
 
         self.radius = abs(math.sin(time * 0.008) - 0.5) * 15 + 350
 
         for i in range(self.num_lines):
             theta = (2 * math.pi / self.num_lines) * i
 
-            x1 = _get_x(theta)
-            y1 = _get_y(theta)
-
-            x2 = _get_x(theta * factor)
-            y2 = _get_y(theta * factor)
+            start_position_line = Position(x=_get_x(theta), y=_get_y(theta))
+            end_position_line = Position(x=_get_x(theta * factor), y=_get_y(theta * factor))
 
             color = "yellow"  # self.get_color()
-            pg.draw.aaline(self.app.screen, color, (x1, y1), (x2, y2))
+            pg.draw.aaline(self.app.screen, color, start_position_line, end_position_line)
 
 
 class App(metaclass=Singleton):
--- src/misc.py
+++ src/misc.py
@@ -1,4 +1,27 @@
-from typing import Any, ClassVar
+from typing import Any, ClassVar, NamedTuple, Protocol
+
+import pygame as pg  # type: ignore
+
+
+class Position(NamedTuple):
+    """Position for view x and y coord."""
+
+    x: int
+    y: int
+
+
+class AppType(Protocol):
+    """Protocol for App object."""
+
+    screen: pg.SurfaceType
+
+    def run(self) -> None:
+        """Protocol method for run app."""
+        ...
+
+    def draw(self) -> None:
+        """Protocol method for drawing app on display."""
+        ...
 
 
 class Singleton(type):
